90dfc3d6ed051201c083893a529cec8bcdc5bb3c
bagbag1278 <bagbag1278@gmail.com>
2026-09-08 08:19:31 -0800

    Upadte Router Task For work with validation and mak a new Folder for validator and handler

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const taskController = require('../controllers/taskController');
+const { getAllTasks, getTaskById, createTask, deleteTask, updateTask, patchTask, toggleTask } = require('../controllers/taskController');
+const {createTaskValidator , updateTaskValidator , patchTaskValidator , idValidator , getAllTasksValidator} = require('../validators/taskValidator');
 
-router.get("/", taskController.getAllTasks);
-router.get("/:id", taskController.getTaskById);
-router.post("/", taskController.createTask);     
-router.delete("/:id", taskController.deleteTask);
-router.put("/:id", taskController.updateTask);
-router.patch("/:id", taskController.patchTask);   
-router.patch("/:id/toggle", taskController.toggleTask);
+router.get("/", getAllTasksValidator, getAllTasks);
+
+router.get("/:id", idValidator, getTaskById);
+
+router.post("/", createTaskValidator, createTask);
+
+router.delete("/:id", idValidator, deleteTask);
+
+router.put("/:id", updateTaskValidator, updateTask);
+
+router.patch("/:id", patchTaskValidator, patchTask);
+
+router.patch("/:id/toggle", idValidator, toggleTask);
 
 module.exports = router;
-
